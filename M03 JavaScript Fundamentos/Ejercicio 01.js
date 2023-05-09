c1b8e5d0516bddf937e9ecc12012d1065a79fdb0
VictorHugoRZ <v.zamoravictor@gmail.com>
2023-05-09 13:36:53 -0600

    prep

--- M03 JavaScript Fundamentos/Ejercicio 01.js
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -2,22 +2,22 @@
 /*1️⃣ EJERCICIO 01 1️⃣*/
 
 // Crea una variable de tipo string.
-const nuevoString = "String";
+const nuevoString = null;
 
 // Crea una variable de tipo number.
-const nuevoNumero = 56;
+const nuevoNumero = null;
 
 // Crea una variable de tipo boolean.
-const nuevoBoolean = true;
+const nuevoBoolean = null;
 
 // Resuelve el siguiente problema matemático.
-const nuevaResta = 10 - 7 === 3;
+const nuevaResta = null === 3;
 
 // Resuelve el siguiente problema matemático.
-const nuevaMultiplicacion = 10 * 4 === 40;
+const nuevaMultiplicacion = null === 40;
 
 // Resuelve el siguiente problema matemático.
-const nuevoModulo = 21 % 5 === 1;
+const nuevoModulo = null === 1;
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
