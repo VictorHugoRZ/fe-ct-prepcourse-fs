7ecffffe27c27882b5f0e03b9512beb7d2375046
VictorHugoRZ <v.zamoravictor@gmail.com>
2023-06-06 17:36:27 -0600

    commit prep

--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -1,74 +1,83 @@
 /*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
 /*5️⃣ EJERCICIO 05 5️⃣*/
 
-function esPositivo(num) {
+function esPositivo(num) { 
    // La función recibe un entero. Devuelve como resultado un string que indica si el número
    // es positivo o negativo.
    // Si el número es positivo ---> "Es positivo".
    // Si el número es negativo ---> "Es negativo".
    // Si el número es 0, devuelve False.
    // Tu código: 
-   
+   if (num > 0) {
+      return "Es positivo";
+   } else if (num < 0) {
+      return "Es negativo";
+   } else {
+      return false;
+   }
 }
 
-function agregarSimboloExclamacion(str) {
+function agregarSimboloExclamacion(str) { //Hola
    // Agrega un símbolo de exclamación al final del string "str" y retórnalo
    // Ejemplo: "hello world" ---> "hello world!"
    // Tu código:
-   
+   return str + "!"; //Hola!
 }
 
-function combinarNombres(nombre, apellido) {
+function combinarNombres(nombre, apellido) { //Pablo Restrepo 
    // Retorna "nombre" y "apellido" combinados en un mismo string pero separados por un espacio.
    // Ejemplo: ("Soy", "Henry") ---> "Soy Henry"
    // Tu código:
-   
+   return nombre + " " + apellido;
 }
-
-//let texto = "texto";
-//let texto2 = 'texto';
-
 
 function obtenerSaludo(nombre) {
    // Toma el string "nombre" y concatena otra string en la cadena para que tome la siguiente forma:
    // Ejemplo: "Martin" ---> "Hola Martin!"
    // Tu código:
-   //return "Hola" + " " + nombre + "!"; //Hola Martin!
-   
+   return "Hola" + " " + nombre + "!"; //Hola Martin!
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
    // Retornar el área de un rectángulo teniendo su altura y ancho.
    // Tu código:
-   
+   var area = alto * ancho;
+   return area;
 }
 
 function retornarPerimetro(lado) {
    // La función recibe como argumento la medida de un lado de un cuadrado.
    // Debes retornar su perímetro.
    // Tu código:
-   
+   var perimetro = lado * 4;
+   return perimetro;
 }
 
 function areaDelTriangulo(base, altura) {
    // Calcula el área de un triángulo y retorna el resultado.
    // Tu código:
-   
+   var area = base * altura / 2;
+   return area;
 }
 
 function deEuroAdolar(euro) {
    // Supongamos que 1 euro equivale a 1.20 dólares.
    // Debes calcular el valor recibido como argumento pasándolo a dolares.
    // Tu código:
-   
+   var conversion = euro * 1.20;
+   return conversion;
 }
 
-function esVocal(letra) {
+function esVocal(letra) {  // &&(and), ||(or)    u
    // Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”.
    // Si el usuario ingresa un string de más de un caracter debes retornar el mensaje: "Dato incorrecto".
    // Si no es vocal, tambien debe retornar "Dato incorrecto".
    // Tu código:
-   
+   if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u") {
+      return "Es vocal";
+   } else {
+      return "Dato incorrecto";
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
